332683545669e3d4c216c169682d903754cf30a7
Daniel JS <daniel@mydeveloperjourney.com>
2022-08-20 14:37:34 -0500

    wip: adds lastest updates

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,26 @@
-function Index(props) {
-    return <h1>Index Page Component</h1>;
+import { Link } from 'react-router-dom';
+
+function Index({ people }) {
+    
+    const loaded = () => {
+        return people.map(({ name, _id }) => {
+            return (
+                <div className="person" key={_id}>
+                    <Link to={`/people/${_id}`}>
+                        <h1>{name}</h1>
+                    </Link>
+                </div>
+            )
+        })
+    };
+    
+    const loading = () => {
+        return <h1>Loading ...</h1>
+    };
+    
+    
+    return people ? loaded() : loading();
 }
+
 
 export default Index;
