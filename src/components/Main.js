6cfe50c4e81e7f1a069004ea2d1fe3c8c8e2dc67
Daniel JS <daniel@mydeveloperjourney.com>
2022-08-20 13:16:33 -0500

    wip: adds routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Index from '../pages/Index';
+import Show from '../pages/Show';
+
 function Main(props) {
-    return <h1>Main Component</h1>;
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Show />} />
+            </Routes>
+        </main>
+    );
 }
 
 export default Main;
